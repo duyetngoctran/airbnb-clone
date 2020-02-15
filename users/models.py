43807b951b34a40ba473fac5b5753af5b07ea6d4
DavidTran <duyet.n.tran@gmail.com>
2020-02-15 15:53:19 +0700

    added room model

--- users/models.py
+++ users/models.py
@@ -15,7 +15,25 @@ class User(AbstractUser):
     )
 
 
-    avatar = models.ImageField(null=True, blank=True)
-    gender = models.CharField(choices=GENDER_CHOICE, max_length=20, null=True, blank=True)
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
 
-    bio = models.TextField(default="", blank=True)
+    LANGUAGE_CHOICES = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_USD = "krw"
+
+    CURRENCY_CHOICES = ((CURRENCY_USD, "USD"), (CURRENCY_USD, "KRW"))
+
+    avatar = models.ImageField(blank=True)
+    gender = models.CharField(choices=GENDER_CHOICE, max_length=20, blank=True)
+
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=True, null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True
+    )
+    superhost = models.BooleanField(default=False)
